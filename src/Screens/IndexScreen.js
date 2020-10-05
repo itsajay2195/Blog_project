0f6dfc1c1a6155f3739f96829891fee2ce3ab4df
Ajaykumar Rajasekaran <ajaykumar.r@happyfox.com>
2020-10-05 12:18:04 +0530

    init

---
+++ src/Screens/IndexScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Text,View,StyleSheet} from 'react-native'
+// 
+
+
+const IndexScreen=()=>{
+    return(
+        <View>
+        <Text>
+            Welcome to the Index Screen
+        </Text>
+        </View>
+    )
+}
+
+const style= StyleSheet.create({
+    
+})
+
+export default IndexScreen;
+
